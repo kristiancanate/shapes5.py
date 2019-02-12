6d62440cf27e1a4a022ba4da5c640c0e1e0e140f
csPython <cspython@UAM-iMAc-AD25TF04NGF1J-16.local>
2019-02-12 11:53:31 -0500

    added first shape

--- shapes5.py
+++ shapes5.py
@@ -3,5 +3,10 @@ from graphics import *
 sWin = GraphWin("5 Shapes", 300, 300)
 sWin.setCoords(0, 0, 300, 300)
 
+#First Object (Pentagon)
+rPen = Polygon(Point(25, 210), Point(10, 260), Point(50, 290), Point(90, 260), Point(75, 210))
+rPen.setFill(color_rgb(230, 30, 30))
+rPen.draw(sWin)
+
 sWin.getMouse()
 sWin.close()
